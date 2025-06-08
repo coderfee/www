9239c129e2531b681373ea8731a697c00e095f0a
coderfee <coderfee@outlook.com>
2025-06-08 16:23:26 +0800

    新增 newsletter RSS 生成器，支持按日期排序的文章列表

---
+++ src/pages/newsletter.xml.js
@@ -0,0 +1,36 @@
+import { loadRenderers } from 'astro:container';
+import { getCollection } from 'astro:content';
+import { SITE_DESCRIPTION, SITE_TITLE } from '@/consts';
+import { getContainerRenderer } from '@astrojs/mdx';
+import rss from '@astrojs/rss';
+import { experimental_AstroContainer as AstroContainer } from 'astro/container';
+
+export async function GET(context) {
+  if (!context.site) {
+    throw new TypeError('context.site falsy');
+  }
+
+  const container = await AstroContainer.create({
+    renderers: await loadRenderers([getContainerRenderer()]),
+  });
+
+  const newsletter = await getCollection('newsletter');
+  const posts = newsletter
+    .sort(({ data: { date: dateA } }, { data: { date: dateB } }) => dateB.valueOf() - dateA.valueOf());
+
+  return rss({
+    title: `${SITE_TITLE} · 明日周刊`,
+    description: `${SITE_DESCRIPTION}`,
+    site: context.site,
+    // stylesheet: '/rss/styles.xsl',
+    items: await Promise.all(
+      posts.map(async (post) => ({
+        ...post.data,
+        pubDate: post.data.date,
+        description: post.data.tldr,
+        link: `/newsletter/${post.slug}/`,
+        content: await container.renderToString((await post.render()).Content),
+      })),
+    ),
+  });
+}
