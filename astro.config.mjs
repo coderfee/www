ff8337879cf2b0300562490f2b13aee1939776e4
coderfee <coderfee@outlook.com>
2025-06-27 21:36:09 +0800

    feat: 字体优化

---
+++ astro.config.mjs
@@ -0,0 +1,83 @@
+import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import pagefind from 'astro-pagefind';
+import { defineConfig, fontProviders } from 'astro/config';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeExternalLinks from 'rehype-external-links';
+import rehypeCodeProps from 'rehype-mdx-code-props';
+import rehypeSlug from 'rehype-slug';
+import remarkToc from 'remark-toc';
+import Icons from 'unplugin-icons/vite';
+
+export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        name: 'LXGWZhenKai',
+        cssVariable: '--font-lxgw',
+        provider: 'local',
+        variants: [
+          {
+            src: ['./src/assets/fonts/LXGWZhenKai-Regular.ttf'],
+          }
+        ],
+      },
+    ],
+  },
+  site: 'https://coderfee.com',
+  publicDir: 'public',
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport',
+  },
+  markdown: {
+    syntaxHighlight: 'shiki',
+    gfm: true,
+    shikiConfig: {
+      theme: 'vitesse-dark',
+      wrap: true,
+    },
+    remarkPlugins: [[remarkToc, { headings: ['h2', 'h3', 'h4'] }]],
+    rehypePlugins: [
+      [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'append' }]],
+      [rehypeCodeProps, []],
+      [
+        rehypeExternalLinks,
+        {
+          target: '_blank',
+        },
+      ],
+    ],
+  },
+  integrations: [
+    react(),
+    mdx(),
+    sitemap({
+      changefreq: 'weekly',
+    }),
+    pagefind(),
+  ],
+  devToolbar: {
+    enabled: false,
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+      Icons({
+        compiler: 'astro',
+      }),
+    ],
+    build: {
+      rollupOptions: {
+        external: ['fsevents'],
+      },
+    },
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
+});
