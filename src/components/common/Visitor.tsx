adbee45fddc78020b68f3f6bb2e08f3cf04f2c4e
coderfee <coderfee@outlook.com>
2025-07-02 21:35:15 +0800

    refactor(Visitor): use useCallback to optimize performance

--- src/components/common/Visitor.tsx
+++ src/components/common/Visitor.tsx
@@ -1,17 +1,17 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { view } from '@/services';
 
 export default function Visitor({ slug, title }) {
   const [viewCount, setViewCount] = useState(0);
 
-  async function recordVisit() {
+  const recordVisit = useCallback(async () => {
     const res = await view({ slug, title });
     setViewCount(res.data || 0);
-  }
+  }, [slug, title]);
 
   useEffect(() => {
     recordVisit();
-  });
+  }, [recordVisit]);
 
   return <>{viewCount}</>;
 }
